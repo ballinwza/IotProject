8fc68915a56a3653c7a1bdeef3ec62e945be7a9f
nunta123 <63603686+nunta123@users.noreply.github.com>
2021-01-25 01:05:07 +0700

    Remove x button

--- src/displayFirebase/pages/Popup.js
+++ src/displayFirebase/pages/Popup.js
@@ -64,9 +64,7 @@ class Popup extends Component  {
                 <div class="modal-content">
                 <div class="modal-header">
                     <h3 class="modal-title" id="exampleModalLabel">Edit Registered Data</h3>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                    </button>
+                    
                 </div>
                 <div class="modal-body">
                     <form onSubmit={this.handleSubmit}>
