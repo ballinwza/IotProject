d7fbfc7d8dabd71bf428ed3c02aa0abca6a35a26
nunta123 <nunta.nuntach@gmail.com>
2021-01-24 17:05:34 +0700

    auto-logout

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,16 +1,28 @@
 export const signIn = (credentials) => {
     return (dispatch, getState, {getFirebase}) =>{
         const firebase = getFirebase();
-        firebase.auth().signInWithEmailAndPassword(
-            credentials.email,
-            credentials.password
-        ).then(()=>{
-            dispatch({type:"LOGIN_SUCCESS"})
-        }).catch((err) => {
-            dispatch({type:'LOGIN_ERROR', err});
-        })
-    }
+        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+  .then(() => {
+    // Existing and future Auth states are now persisted in the current
+    // session only. Closing the window would clear any existing state even
+    // if a user forgets to sign out.
+    // ...
+    // New sign-in will be persisted with session persistence.
+    return firebase.auth().signInWithEmailAndPassword(
+        credentials.email,
+        credentials.password
+    ).then(()=>{
+        dispatch({type:"LOGIN_SUCCESS"})
+    })
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    dispatch({type:'LOGIN_ERROR', error});
+  });
+       
 }
+}
+
 
 export const signOut = () => {
     return (dispatch,getState, {getFirebase}) => {
