39b26d57bc305a19f35bec1b4c411cc592e78681
nunta123 <63603686+nunta123@users.noreply.github.com>
2021-01-25 01:13:39 +0700

    Update Patient.js

--- src/displayFirebase/pages/Patient.js
+++ src/displayFirebase/pages/Patient.js
@@ -11,17 +11,55 @@ import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 import LineChart from '../myCustomModules/LineChart';
 import { type } from 'jquery';
+//pop-up
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import Popup from "./Popup";
+import updateFirestore from '../../actions/updatePatientAction'
+
 
 class Patient extends Component {
-
+    state = {
+        leftColHeader:this.props.leftColHeader
+    }
     getParams(){
         let url = this.props.location.search;
         let params = queryString.parse(url);
+        if (this.state.leftColHeader.name == null) {
+            this.setState({
+                leftColHeader:{
+                    name:params.name,
+                    gender:params.gender,
+                    date:'',
+                    time:'',
+                    activity:'',
+                    uuid:params.uuid
+                }
+            })
+        }
         return params;
     }
+    handleSubmitPopup = (updatedState) => {
+        //change state
+        console.log("handleChangePopup:",updatedState)
+        this.setState(prevState => {
+            let leftColHeader = { ...prevState.leftColHeader };  // creating copy of state variable jasper
+            leftColHeader.date = updatedState.date;                     // update the name property, assign a new value                 
+            leftColHeader.time = updatedState.time; 
+            leftColHeader.activity = updatedState.activity; 
+            console.log(leftColHeader)
+            return { leftColHeader };                                 // return new object jasper object
+          })
 
+        //update firebase
+        let uuid = this.state.leftColHeader.uuid
+        const data = updatedState;
+        console.log(uuid,data)
+        this.props.updateFirestore(uuid,data,firebaseApp);
+    }
     componentDidMount(){
         this.queryDataFromFireStore();
+        this.queryPatientDataFromFireStore()
     }
     queryDataFromFireStore(){
         const fs = firebaseApp.firestore();
@@ -33,10 +71,29 @@ class Patient extends Component {
             })
         });
     }
+    queryPatientDataFromFireStore(){
+        const fs = firebaseApp.firestore();
+        const db = fs.collection("patients");
+        const query = db.doc(this.getParams().uuid);
+        query.onSnapshot((doc) => {
+            if (doc.data()!=null){
+                this.setState(prevState => {
+                    let leftColHeader = { ...prevState.leftColHeader };  // creating copy of state variable jasper
+                    leftColHeader.date = doc.data().date;                     // update the name property, assign a new value                 
+                    leftColHeader.time = doc.data().time;  
+                    leftColHeader.activity = doc.data().activity;  
+                    return { leftColHeader };                                 // return new object jasper object
+                })
+            }
+        });
+    }
 
     RegisteredData(){
         const { uid } = this.props;//WOW!! no need to so sth like this.props.authError
         if (!uid) return (<Redirect to = '/' />)
+        //pop-up
+        let leftColHeader = this.state.leftColHeader
+
         return(
             <div className="col-12 col-md-6">
                 <div className="registerData-container">
@@ -44,28 +101,37 @@ class Patient extends Component {
                     <div className="detail">
                         <div className="row textContain">
                             <div className="col-2 head">ชื่อ</div>
-                            <div className="col-10">{this.getParams().name}</div>
+                            <div className="col-10">{leftColHeader.name}</div>
                         </div>
                         <div className="row textContain">
                             <div className="col-2 head">เพศ</div>
-                            <div className="col-10">{this.getParams().gender}</div>
+                            <div className="col-10">{leftColHeader.gender}</div>
                         </div>
                         <div className="row textContain">
                             <div className="col-2 head">date</div>
-                            <div className="col-10">{this.getParams().date}</div>
+                            <div className="col-10">{leftColHeader.date}</div>
                         </div>
                         <div className="row textContain">
                             <div className="col-2 head">time</div>
-                            <div className="col-10">{this.getParams().time}</div>
+                            <div className="col-10">{leftColHeader.time}</div>
                         </div>
                         <div className="row textContain">
                             <div className="col-2 head">activity</div>
-                            <div className="col-10">{this.getParams().activity} mCi</div>
+                            <div className="col-10">{leftColHeader.activity} mCi</div>
                         </div>
+                        <div className="card-body">
+                            <button className="btn-download" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">
+                            <FontAwesomeIcon icon={faEdit} />
+                            Update
+                            </button>
+                        </div>
+            
+            
+                        <br/>
                     </div>
+                     <Popup previousData={this.state.leftColHeader} handleChange={this.handleChangePopup} handleSubmit={this.handleSubmitPopup}/>
                     {this.QRCode()}
                 </div>
-                
             </div>
         )
     }
@@ -112,7 +178,7 @@ class Patient extends Component {
 
     showPatient(){
         if (this.state !== null && this.state !== undefined){
-            let dbArray = Object.entries(this.state).sort(sortFunctionByDateTime).reverse();
+            let dbArray = Object.entries(this.state).filter(dbArray => dbArray[0] != "leftColHeader").sort(sortFunctionByDateTime).reverse();
             return dbArray.map((patient, index) => (
                 <div className="row" key={index}>
                     <div className="col-2 col-md-1 detailTable">{index + 1}</div>
@@ -141,8 +207,15 @@ class Patient extends Component {
 
 const mapStateToProps = (state) => {
     return{
-        uid : state.firebase.auth.uid
+        uid : state.firebase.auth.uid,
+        leftColHeader : state.patient
+        
+    }
+}
+const mapDispatchToProps = (dispatch) =>{
+    return {
+        updateFirestore: (id, data,firebaseApp) => dispatch(updateFirestore(id, data,firebaseApp))
     }
 }
 
-export default connect(mapStateToProps)(Patient);
+export default connect(mapStateToProps,mapDispatchToProps)(Patient);
